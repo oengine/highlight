e2fe114b85b0c3c479d8da5e64b20aa5ad26e3cf
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-01-05 13:03:58 +0700

    support laravel

---
+++ src/HighlightServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OEngine\Highlight;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class HighlightServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        Blade::directive('highlight', function ($theme) {
+            return "
+            <?php ob_start(); ?>"; //return this if statement inside php tag
+        });
+
+        Blade::directive('endhighlight', function ($theme) {
+            return "
+            <?php
+            \$__highlights = ob_get_clean();
+            echo \OEngine\Highlight\FormatCode::Highlight(\$__highlights,{$theme}));
+            ?>
+            "; //return this endif statement inside php tag
+        });
+    }
+}
