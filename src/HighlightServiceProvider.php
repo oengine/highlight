3b0c9cf815fc01c4fc54f7622e5d856453fe6992
nguyen van hau <nguyenvanhau.soft@gmail.com>
2023-01-05 17:26:02 +0700

    Update HighlightServiceProvider.php

--- src/HighlightServiceProvider.php
+++ src/HighlightServiceProvider.php
@@ -11,14 +11,22 @@ class HighlightServiceProvider extends ServiceProvider
     {
         Blade::directive('highlight', function ($theme) {
             return "
-            <?php ob_start(); ?>"; //return this if statement inside php tag
+            
+            <?php
+            \$__highlights_option = [{$theme}];
+            if(count(\$__highlights_option)==0)
+            {
+                \$__highlights_option = [\"\"];
+            }
+            
+            ob_start(); ?>"; //return this if statement inside php tag
         });
 
-        Blade::directive('endhighlight', function ($theme='html') {
+        Blade::directive('endhighlight', function () {
             return "
             <?php
             \$__highlights = ob_get_clean();
-            echo \OEngine\Highlight\FormatCode::Highlight(\$__highlights,{$theme});
+            echo \OEngine\Highlight\FormatCode::Highlight(\$__highlights,...\$__highlights_option);
             ?>
             "; //return this endif statement inside php tag
         });
